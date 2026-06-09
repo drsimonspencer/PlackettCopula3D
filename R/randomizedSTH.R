dae8068d0f3241168670f32b15ca51731cca018f
Simon Spencer <46009220+drsimonspencer@users.noreply.github.com>
2026-06-09 12:10:55 +0100

    Add files via upload

---
+++ R/randomizedSTH.R
@@ -0,0 +1,14 @@
+#' Randomized STH dataset
+#'
+#' A bootstrap sample (conditioned on matching the marginal prevalences) from the dataset used in
+#' Bayesian analysis of co-infection with multiple, interacting, soil-transmitted helminths in humans
+#' using the trivariate Plackett copula.
+#'
+#' The order of the helminths is RW/HW/WW, ie Ascaris/Hookworm/Trichuris.
+#'
+#' @references #' Bayesian analysis of co-infection with multiple, interacting, soil-transmitted helminths in humans
+#' using the trivariate Plackett copula.
+#' Giorgia Lepschy, Matthew Adeoye, Thomas Crellen, Evandro Konzen, Sharmini Gunawardena, Claudio Nunes Alves,
+#' Vachel Gay Paller, Joaquin M Prada, T. Déirdre Hollingsworth, Simon E.F. Spencer (2026)
+#'
+'randomizedSTH'
