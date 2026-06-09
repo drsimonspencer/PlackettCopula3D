13411a827d062d1390ab61173439cefef903a292
Simon Spencer <46009220+drsimonspencer@users.noreply.github.com>
2026-06-09 09:47:52 +0100

    SriLanka data added

---
+++ R/SriLankaSTH.R
@@ -0,0 +1,10 @@
+#' STH data from Sri Lanka
+#'
+#' The order of the helminths is RW/HW/WW, ie Ascaris/Hookworm/Trichuris.
+#'
+#' @references Complex interactions in soil-transmitted helminth co-infections from a cross-sectional study in Sri Lanka.
+#'
+#' Hannah C Lepper, Joaquín M Prada, Emma L Davis, Sharmini A Gunawardena, T Déirdre Hollingsworth
+#'
+#' Transactions of The Royal Society of Tropical Medicine and Hygiene, Volume 112, Issue 8, August 2018, Pages 397–404. \url{https://academic.oup.com/trstmh/article/112/8/397/5059392}
+'SriLankaSTH'
