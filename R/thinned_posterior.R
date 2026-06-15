1598c40cb74273652a709fb7e02427e7fc2b3b6a
Simon Spencer <46009220+drsimonspencer@users.noreply.github.com>
2026-06-15 15:37:21 +0100

    Add files via upload

---
+++ R/thinned_posterior.R
@@ -0,0 +1,14 @@
+#' Thinned posterior samples for the STH dataset used in the paper below.
+#'
+#' The thinning factor was 5, and the burn-in 100, leaving 2000 posterior samples.
+#'
+#' The posterior distribution can be used to simulate prevalence (such as anySTH) from marginal prevalence data.
+#'
+#' The order of the helminths is RW/HW/WW, ie Ascaris/Hookworm/Trichuris.
+#'
+#' @references #' Bayesian analysis of co-infection with multiple, interacting, soil-transmitted helminths in humans
+#' using the trivariate Plackett copula.
+#' Giorgia Lepschy, Matthew Adeoye, Thomas Crellen, Evandro Konzen, Sharmini Gunawardena, Claudio Nunes Alves,
+#' Vachel Gay Paller, Joaquin M Prada, T. Déirdre Hollingsworth, Simon E.F. Spencer (2026)
+#'
+'thinned_posterior'
